44aa777cc6175eba5345dde52a308777fde31893
Emiliobau <emiliobau@hotmail.com>
2021-10-25 18:00:42 -0300

    agregando iconos Font Awesome

--- dashboard/src/components/SideBar.js
+++ dashboard/src/components/SideBar.js
@@ -1,34 +1,36 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faChartLine } from "@fortawesome/free-solid-svg-icons"
+import { faTachometerAlt } from "@fortawesome/free-solid-svg-icons"
 
 function SideBar(){
     return(
-		<div >	
-			
+		<div >
+
 		<ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-			
+
 			<a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
 				<div className="sidebar-brand-icon"></div>
-				
-				
+						<FontAwesomeIcon icon={faChartLine} />
 				<div className="sidebar-brand-text mx-3">Admin</div>
 			</a>
-		
+
 			<hr className="sidebar-divider"/>
-				
+
 			<li className="nav-item active">
 				<a className="nav-link" href="/">
-					<i className="fas fa-fw fa-tachometer-alt"></i>
+							<FontAwesomeIcon icon={faTachometerAlt} />
 					<span>Dashboard</span></a>
 			</li>
 
-			
+
 			<hr className="sidebar-divider"/>
 
-			
+
 			<div className="sidebar-heading">Actions</div>
 
-			
+
 			<li className="nav-item">
 				<a className="nav-link collapsed" href="/">
 					<i className="fas fa-fw fa-folder"></i>
@@ -36,25 +38,25 @@ function SideBar(){
 				</a>
 			</li>
 
-			
+
 			<li className="nav-item">
 				<a className="nav-link" href="/">
 					<i className="fas fa-fw fa-chart-area"></i>
 					<span>Charts</span></a>
 			</li>
 
-			
+
 			<li className="nav-item">
 				<a className="nav-link" href="/">
 					<i className="fas fa-fw fa-table"></i>
 					<span>Tables</span></a>
 			</li>
 
-			
-			
-		</ul>  
-	</div>	
-    
+
+
+		</ul>
+	</div>
+
     );
 }
 export default SideBar;
